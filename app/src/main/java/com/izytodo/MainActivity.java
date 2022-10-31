c50159a0f95227ad7c39e7c1da5aa5d682bbb9a1
channugraha <nugrahaproduction70@gmail.com>
2022-10-31 08:42:12 +0700

    membuat homepage

---
+++ app/src/main/java/com/izytodo/MainActivity.java
@@ -0,0 +1,16 @@
+package com.izytodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
